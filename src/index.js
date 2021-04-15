0cb6b1a2346be9461521e62cd85dd9e1bd2e9cd2
Méloé <meloscavezzoni@gmail.com>
2021-04-15 20:26:10 +0200

    GSAP animation for banner

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import Rellax from 'rellax';
+import { gsap } from "gsap";
 
 const rellax = new Rellax(".rellax");
+
+const logo = document.querySelector('.logo-container svg');
+const text = document.querySelectorAll(".banner-text .anim");
+const icons = document.querySelectorAll('.icons .icon-anim');
+
+window.addEventListener('load', () => {
+    const TL = gsap.timeline({paused: true});
+
+    TL
+    .from(logo,{duration: 3,stagger:0.3,opacity: 0, ease: "power2.out"})
+    .from(text,{duration: 2, stagger:0.3 ,opacity: 0, ease: "power2.out"},'-=2')
+    .from(icons,{duration: 0.4, stagger:0.3 ,scale:0, ease:"power2.out"},'-=1');
+    
+    TL.play();
+})
