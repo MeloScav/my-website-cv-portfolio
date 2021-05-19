5f86c8fe86e6443170bd83bd1ce02a5cc8edccb1
Méloé <meloscavezzoni@gmail.com>
2021-05-19 18:23:23 +0200

    Modification of the header animation

--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
-import Rellax from 'rellax';
+import Rellax from "rellax";
 import { gsap } from "gsap";
 
 const rellax = new Rellax(".rellax");
 
-const logo = document.querySelector('.logo-container svg');
+const logo = document.querySelector(".logo-container img");
 const text = document.querySelectorAll(".banner-text .anim");
-const icons = document.querySelectorAll('.icons .icon-anim');
+const icons = document.querySelectorAll(".icons .icon-anim");
 
-window.addEventListener('load', () => {
-    const TL = gsap.timeline({paused: true});
+window.addEventListener("load", () => {
+  const TL = gsap.timeline({ paused: true });
 
-    TL
-    .from(logo,{duration: 3,stagger:0.3,opacity: 0, ease: "power2.out"})
-    .from(text,{duration: 2, stagger:0.3 ,opacity: 0, ease: "power2.out"},'-=2')
-    .from(icons,{duration: 0.4, stagger:0.3 ,scale:0, ease:"power2.out"},'-=1');
-    
-    TL.play();
-})
+  TL.to(logo, {
+    duration: 2,
+    autoAlpha: 1,
+    y: 0,
+    ease: "bounce.out",
+  })
+    .to(text, { duration: 1.5, stagger: 0.4, autoAlpha: 1, ease: "power2.out" })
+    .to(icons, {
+      duration: 0.4,
+      stagger: 0.3,
+      scale: 1,
+      autoAlpha: 1,
+      ease: "power2.out",
+    });
+  TL.play();
+});
